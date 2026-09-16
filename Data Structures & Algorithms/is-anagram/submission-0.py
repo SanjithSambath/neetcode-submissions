549e64104317ee19ebaa14109b281aa61e856bee
SanjithSambath <sanjith.sambath07@gmail.com>
2026-09-16 16:05:15 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        hash1 = {}
+        hash2 = {}
+
+        for char in s:
+
+            if char not in hash1:
+                hash1[char] = 0
+
+            hash1[char] += 1
+        
+        for char in t:
+
+            if char not in hash2:
+                hash2[char] = 0
+
+            hash2[char] += 1
+
+        if hash1 == hash2:
+            return True
+
+        return False
